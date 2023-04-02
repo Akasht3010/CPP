42a99de0480645b9e49742b0889661646d875780
akasshh30 <akashthakkar931@gmail.com>
2023-04-03 00:10:51 +0530

    right triangle pattern

---
+++ Right_triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            if(j<=n-i){
+                cout<<"  ";
+            }
+            else{
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
