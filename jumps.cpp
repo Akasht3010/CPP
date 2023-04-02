474539f8e3ce1f643abe73b8eaae43aba71d5021
akasshh30 <akashthakkar931@gmail.com>
2023-04-03 00:08:33 +0530

    contnue and break in if else

---
+++ jumps.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int pocketMoney=3000;
+    for(int date=1;date<=30;date++){
+        if(date%2==0){
+            continue;
+        }
+        if(pocketMoney==0){
+            break;
+        }
+        cout<<"Go out today!"<<endl;
+        pocketMoney=pocketMoney-300;
+    }
+}
