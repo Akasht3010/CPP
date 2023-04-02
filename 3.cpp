6706d9b2e97a2daca21cb182e3cf101b63c73fb0
akasshh30 <akashthakkar931@gmail.com>
2023-04-03 00:02:27 +0530

    a number rasied to a power

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"a is"<<a<<endl;
+    cout<<"b is"<<b<<endl;
+    cout<<pow(a,b)<<"is the value of number raised to it's power"<<endl;
+    cout<<"b is"<<b<<endl;
+
+}
