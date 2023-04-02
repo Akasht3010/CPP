b64e20849ff70d92420a7f9e3f49b5ba06007603
akasshh30 <akashthakkar931@gmail.com>
2023-04-03 00:13:22 +0530

    vectors

---
+++ vector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int mainf / Ways to create a vector vector‹int > vec1; // zero length integer vector vector ‹char> vec2(4); // 4-element character vector
+    // vec2.push_back('5');
+    // display (vec2);
+    // vector<char> vec3 (vec2); // 4-element character vector from vec2
+    / display(vec3);
+    vector‹int > vec4(6, 13); // 6-element vector of 3s display (vec4);
+    int element, size = 5;
+    / cout << "Enter the size of your vector" << endl;
+    / cin››size;
+    // for (int i = 0; i < size; i++)
+    // {
+    cout <‹"Enter an element to add to this vector:
+        cin››element;
+    vec1.push_back(element);
+    /
+}
+// vec1.pop_back();
+// display (vec1);
+// vector<int›:: iterator iter = vec1. begin() ;
+// // vec1. insert (iter+1,500, 566);
+/ display(vec1);
+
+return 0;
+}
