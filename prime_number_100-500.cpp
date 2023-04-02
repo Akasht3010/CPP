761fbc09ce408aee8dbd25d2da87f9f24f7496c0
akasshh30 <akashthakkar931@gmail.com>
2023-04-03 00:11:18 +0530

    prime number

---
+++ prime_number_100-500.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for(int i=100;i<=500;i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
